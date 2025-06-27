dd0fd2f14ddea7c530b1251f163e8609e193eae6
Umar-Farooq007 <umarfarooq17036@gmail.com>
2025-06-27 09:58:54 +0530

    created a database of products

--- e_website/product/admin.py
+++ e_website/product/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import product   #. dot stands for current directory
 # Register your models here.
+admin.site.register(product) #Replace with your model when they define
