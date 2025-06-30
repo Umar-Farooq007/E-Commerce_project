33ebd82e59773aa000dd13972daf2ae5ba5a1bb5
Umar-Farooq007 <umarfarooq17036@gmail.com>
2025-06-30 15:12:50 +0530

    Database product update

--- e_website/product/models.py
+++ e_website/product/models.py
@@ -5,3 +5,6 @@ class product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.name 
